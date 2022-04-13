b1037fa513c299fa0e72b715e82fbd6655625fa3
hermanlyx <hermanlyx@gmail.com>
2022-04-13 23:04:43 +0800

    fleshed out validation and installed yup resolver

--- src/components/FormValidation.js
+++ src/components/FormValidation.js
@@ -1,13 +1,28 @@
 import * as yup from "yup";
 
 const accountSchema = yup.object().shape({
-  name: yup.string().required("please enter your investment horizon"),
-  initalAmount: yup.number().required(),
-  monthlyDeposit: yup.number().required(),
-  interest: yup.number().required(),
+  name: yup.string().required("Please enter your investment horizon"),
+  initalAmount: yup
+    .number()
+    .round("round")
+    .required("Please enter your initial amount"),
+  monthlyDeposit: yup
+    .number()
+    .round("round")
+    .required("Please enter your monthly deposit"),
+  interest: yup
+    .number()
+    .round("round")
+    .required("Please enter your projected interest"),
 });
 
 export const formSchema = yup.object().shape({
-  investmentHorizon: yup.number().required(),
-  accounts: yup.array.of(accountSchema).required(),
+  investmentHorizon: yup
+    .number()
+    .positive()
+    .required("Please enter your invesment horizon"),
+  accounts: yup
+    .array()
+    .of(accountSchema)
+    .min(1, "Please add at least 1 account"),
 });
