29c8c08b9709b06791cce8d538aeaa8a09c93e73
hermanlyx <hermanlyx@gmail.com>
2022-04-14 01:08:25 +0800

    added some changes to validation

--- src/components/FormValidation.js
+++ src/components/FormValidation.js
@@ -4,16 +4,17 @@ const accountSchema = yup.object().shape({
   name: yup.string().required("Please enter your investment horizon"),
   initialAmount: yup
     .number()
-    .typeError("Amount must be a number")
+    .typeError("Initial amount must be a number")
     .round("round")
     .required("Please enter your initial amount"),
   monthlyDeposit: yup
     .number()
-    .typeError("Amount must be a number")
+    .typeError("Monthly deposit must be a number")
     .round("round")
     .required("Please enter your monthly deposit"),
   interest: yup
     .number()
+    .typeError("Projected interest must be a number")
     .round("round")
     .required("Please enter your projected interest"),
 });
@@ -21,7 +22,7 @@ const accountSchema = yup.object().shape({
 export const formSchema = yup.object().shape({
   investmentHorizon: yup
     .number()
-    .typeError("Amount must be a number")
+    .typeError("Investment horizon must be a number")
     .positive("Please enter a positive value")
     .required("Please enter your invesment horizon"),
   accounts: yup
